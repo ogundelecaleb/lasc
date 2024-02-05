1d086018b9f702b05a1a06d5c923db64ee9b446d
Ogundele Caleb <ogundelecaleb13@gmail.com>
2024-02-05 17:23:41 +0100

    fixed route issue

--- src/scene/home/index.jsx
+++ src/scene/home/index.jsx
@@ -3,5 +3,5 @@ import { Navigate } from "react-router-dom";
 
 export default function HomeIndex(){
   
-  return  <Navigate to="/dashboard" replace />;
+  return  <Navigate to="/clearance" replace />;
 }
